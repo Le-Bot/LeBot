ccc3a6e05d64c4ee944bcbf7f0ba73c78afbc70d
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-20 21:09:22 +0530

    Config updated to get Slack Bot tokens from environment

--- lebot/config.py
+++ lebot/config.py
@@ -1,5 +1,10 @@
+import os
 from bot import Bot
 
-SLACK_BOT = Bot()
+SLACK_BOT = Bot(
+    token=os.environ.get('SLACK_BOT_TOKEN'),
+    name=os.environ.get('SLACK_BOT_NAME'),
+    id=os.environ.get('SLACK_BOT_ID'),
+)
 
 WEB_SOCKET_POLL_DELAY = 1
