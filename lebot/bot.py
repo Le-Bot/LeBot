3acf713e83ca95ef36cc2671f629cbf11ec6062a
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-20 20:35:32 +0530

    Bot class

---
+++ lebot/bot.py
@@ -0,0 +1,15 @@
+class Bot(object):
+    def __init__(self, **kwargs):
+        self.id = kwargs.get('id')
+        self.name = kwargs.get('name')
+        self.token = kwargs.get('token')
+        self.at = '<@{user}>'.format(user=self.id)
+
+    def is_me(self, name):
+        return self.name == name
+
+    def is_mentioned(self, text):
+        return self.at in text
+
+    def is_by_me(self, id):
+        return self.id == id
