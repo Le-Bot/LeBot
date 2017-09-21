c53d4dcdf4255afd3d5660b710f57351bb9cda97
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-21 19:44:47 +0530

    Factory class updated for two case i.e. Event or RTM

--- lebot/factory.py
+++ lebot/factory.py
@@ -2,16 +2,18 @@ from slackclient import SlackClient
 from cerebro import cerebro
 import bot, config, helper as hlpr
 
+SLACK_EVENT_BOT = 1
+SLACK_RTM_BOT = 2
 
-def slack_bot():
+
+def slack_bot(bot_type):
     cfg = config.CONFIG
 
     slack_client = SlackClient(cfg.get('SLACK_BOT').token)
 
     helper = hlpr.SlackHelper(client=slack_client)
 
-    return bot.SlackBot(
-        config=config.CONFIG,
-        helper=helper,
-        cerebro=cerebro
-    )
+    if bot_type == SLACK_EVENT_BOT:
+        return bot.SlackEventBot(config=config.CONFIG, helper=helper, cerebro=cerebro)
+    else:
+        return bot.SlackRTMBot(config=config.CONFIG, helper=helper, cerebro=cerebro)
