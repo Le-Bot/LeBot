dcbe54d6cc6ff15ca3f7f3e36522545a34dcfe9e
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-21 19:42:55 +0530

    Config updated for verification token

--- lebot/config.py
+++ lebot/config.py
@@ -7,6 +7,7 @@ CONFIG = {
         token=os.environ.get('SLACK_BOT_TOKEN'),
         name=os.environ.get('SLACK_BOT_NAME'),
         id=os.environ.get('SLACK_BOT_ID'),
+        verification_token=os.environ.get('VERIFICATION_TOKEN'),
     ),
     'WEB_SOCKET_POLL_DELAY': 1
 }
