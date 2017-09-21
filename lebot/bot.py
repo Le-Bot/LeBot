69568d0a7daad066cce363513aa6a39f4b47e5fb
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-21 19:44:19 +0530

    Bot updated: - Refactored command functions into SlackBot class - Two specialised class added: SlackEventBot, SlackRTMBot - Respective class have respective implementation

--- lebot/bot.py
+++ lebot/bot.py
@@ -20,27 +20,37 @@ class SlackBot(object):
                 print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
                 self.user.id = user.get('id')
 
-    def check_output(self, output):
-        return output \
-               and output.get('type') == "message" \
-               and not self.user.is_by_me(output.get('user'))
+    def validate(self, event):
+        return event \
+               and event.get('type') == "message" \
+               and not self.user.is_by_me(event.get('user'))
 
-    def parse_slack_output(self, slack_rtm_output):
-        output_list = slack_rtm_output
-
-        if not output_list and len(output_list) <= 0:
+    def parse_input_event(self, event):
+        if not self.validate(event):
             return None, None
 
-        for output in output_list:
-            if not self.check_output(output):
-                continue
+        text, channel = event.get('text'), event.get('channel')
 
-            text, channel = output.get('text'), output.get('channel')
+        if self.user.is_mentioned(text=text):
+            text = text.split(self.user.at)[1].strip().lower()
 
-            if self.user.is_mentioned(text=text):
-                text = text.split(self.user.at)[1].strip().lower()
+        return text, channel
 
-            return text, channel
+
+class SlackRTMBot(SlackBot):
+
+    def parse_rtm_message(self, rtm_message):
+        event_list = rtm_message
+
+        if not event_list and len(event_list) <= 0:
+            return None, None
+
+        for event in event_list:
+            text, channel = self.parse_input_event(event=event)
+
+            if text and channel:
+                return text, channel
+
         return None, None
 
     def run(self):
@@ -51,9 +61,25 @@ class SlackBot(object):
         print("StarterBot connected and running!")
 
         while True:
-            command, channel = self.parse_slack_output(self.helper.read())
+            command, channel = self.parse_rtm_message(self.helper.read())
 
             if command and channel:
                 self.handle_command(command, channel)
 
             time.sleep(self.config.get('WEB_SOCKET_POLL_DELAY'))
+
+
+class SlackEventBot(SlackBot):
+    def execute(self, event, token):
+        if not self.user.token_verification(token=token):
+            return "Invalid Slack verification token: ", 403, {"X-Slack-No-Retry": 1}
+
+        if event:
+            command, channel = self.parse_input_event(event=event)
+
+            if command and channel:
+                self.handle_command(command, channel)
+
+            return "Success", 200, {"X-Slack-No-Retry": 1}
+
+        return "Invalid Event", 404, {"X-Slack-No-Retry": 1}
