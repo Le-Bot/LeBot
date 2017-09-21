1179b1e07b72e5d734eefab760c4d7ab9cc588a0
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2017-09-21 19:46:03 +0530

    - App class added for HTTP APIs - its a basic flask app with one end point

---
+++ lebot/app.py
@@ -0,0 +1,22 @@
+import json
+
+from flask import Flask, request, make_response, render_template
+
+import factory
+
+app = Flask(__name__)
+
+
+@app.route("/listening", methods=["GET", "POST"])
+def hears():
+    slack_event = json.loads(request.data)
+
+    if "challenge" in slack_event:
+        return make_response(slack_event["challenge"], 200, {"content_type": "application/json"})
+
+    bot = factory.slack_bot(bot_type=factory.SLACK_EVENT_BOT)
+    return make_response(bot.execute(event=slack_event.get('event'), token=slack_event.get('token')))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
